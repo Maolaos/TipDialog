f4d0a9fef81d4e8c065706ffa8049543ce8ac4d2
jiaoyanan <jiao_ya_nan@163.com>
2019-04-08 09:07:22 +0800

    Initial commit

---
+++ MyTipdialog/src/main/java/com/jyn/mytipdialog/dialog/DisplayHelper.java
@@ -0,0 +1,135 @@
+/*
+ * Tencent is pleased to support the open source community by making QMUI_Android available.
+ *
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jyn.mytipdialog.dialog;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.net.ConnectivityManager;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Locale;
+
+/**
+ * @author cginechen
+ * @date 2016-03-17
+ */
+public class DisplayHelper {
+
+    /**
+     * 屏幕密度,系统源码注释不推荐使用
+     */
+    public static final float DENSITY = Resources.getSystem()
+            .getDisplayMetrics().density;
+    private static final String TAG = "DisplayHelper";
+
+
+
+    /**
+     * 把以 dp 为单位的值，转化为以 px 为单位的值
+     *
+     * @param dpValue 以 dp 为单位的值
+     * @return px value
+     */
+    public static int dpToPx(int dpValue) {
+        return (int) (dpValue * DENSITY + 0.5f);
+    }
+
+    /**
+     * 把以 px 为单位的值，转化为以 dp 为单位的值
+     *
+     * @param pxValue 以 px 为单位的值
+     * @return dp值
+     */
+    public static int pxToDp(float pxValue) {
+        return (int) (pxValue / DENSITY + 0.5f);
+    }
+
+    public static float getDensity(Context context) {
+        return context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float getFontDensity(Context context) {
+        return context.getResources().getDisplayMetrics().scaledDensity;
+    }
+
+
+    /**
+     * 单位转换: dp -> px
+     *
+     * @param dp
+     * @return
+     */
+    public static int dp2px(Context context, int dp) {
+        return (int) (getDensity(context) * dp + 0.5);
+    }
+
+    /**
+     * 单位转换: sp -> px
+     *
+     * @param sp
+     * @return
+     */
+    public static int sp2px(Context context, int sp) {
+        return (int) (getFontDensity(context) * sp + 0.5);
+    }
+
+    /**
+     * 单位转换:px -> dp
+     *
+     * @param px
+     * @return
+     */
+    public static int px2dp(Context context, int px) {
+        return (int) (px / getDensity(context) + 0.5);
+    }
+
+    /**
+     * 单位转换:px -> sp
+     *
+     * @param px
+     * @return
+     */
+    public static int px2sp(Context context, int px) {
+        return (int) (px / getFontDensity(context) + 0.5);
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/jyn/tipdialog/MainActivity.java
@@ -0,0 +1,21 @@
+package com.jyn.tipdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jyn.mytipdialog.dialog.TipDialog;
+
+import java.util.Queue;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
